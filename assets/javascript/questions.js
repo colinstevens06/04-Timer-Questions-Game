d18af4d7a2d23deae7ac21d34233be2f562f2586
Colin Stevens <colinstevens06@gmail.com>
2019-11-08 16:25:34 -0500

    working on the javascript to add questions to the page and take away time for wrong answers

---
+++ assets/javascript/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+   {
+     title: "Who is not currently a member of blink-182?",
+     choices: ["Mark Hoppus", "Tom Delonge", "Travis Barker", "Matt Skiba"],
+     answer: "Tom Delonge"
+   },
+   {
+     title: "Which song is by Green Day",
+     choices: ["Dookie", "Dammit", "Don't Let Go", "Disenchanted Lullaby"],
+     answer: "Dookie"
+   },
+ ];
+
+console.log(questions[0].choices[2])
---
+++ assets/javascript/script.js
@@ -0,0 +1,88 @@
+// create a timer
+// that timer is also going to have to correlate to the score
+// so when the last question is answered, the timer should stop and the time left on the timer should retun value to the score
+// when someone gets an answer wrong - going to have to be like true/false statements for the questions, returning true/false to a variable, maybe that variable is 'answer question 1' etc ... and if the value is true you just move to the next question and get a <hr> w correct promp. if answer returns false, the timer has to immediately go down by 15 seconds
+// after answering the questions, the timer stops, and that value is returned to 'score'
+// then the user enters initials, clicks submit button, and name/score are stored in the high scores section
+// the high scores section is going to append the score and resort so that the highest score is on top, so it probably appends to an object, that object is sorted based on the score key, and then it's actually displayed
+
+
+// ************ DECLARE VARIABLES HERE *************
+var score;
+var timeLeft;
+var interval;
+var totalSeconds = 75;
+
+var timerDiv = document.getElementById("timer");
+var questionBoxDiv = document.getElementById("question-box");
+var questionDiv = document.getElementById("question");
+var answerChoicesDiv = document.getElementById("answer-choices");
+var rightOrWrongDiv = document.getElementById("right-or-wrong");
+var startButton = document.getElementById("start-button");
+
+
+
+
+// ************ WRITE FUNCTIONS ************
+
+// ***** TIMER *****
+function startTimer() {
+   var timerInterval = setInterval(function () {
+      totalSeconds--;
+      timerDiv.textContent = totalSeconds;
+
+      if (totalSeconds === 0) {
+         clearInterval(timerInterval);
+         alert("Game Over")
+         // need to put a 'game over' message here
+      };
+   }, 1000);
+}
+
+// ***** QUESTIONS/QUIZ *****
+// ** PSEUDO CODE FOR QUESTIONS/QUIS **
+/*
+okay so i'm going to be loping through my questions object. it's going to hit the first loop, and you only get 15 seconds to answer the question, so some timer will have to start, it's going to pull the questions[i].title and place it in the div that contains the the questions, then it's going to create 4 buttons, and each button is going to contain a question questions[i].choices[1-4], per the GIF provided as an example, clicking a button take the user to the next question. if the answer is correct, give message for correct. if answer is wrong, give message for wrong and deduct 15 seconds from the timer. when last question is answered, display the score and store it (seperate code for all that)
+*/
+
+function quizTime() {
+
+   for (i = 0; i < questions.length; i++) {
+      questionBoxDiv.textContent = questions[i];
+      for (j = 0; j < 4; j++) {
+         var answerButton = answerChoicesDiv.createElement("button");
+         answerButton.textContent = question[i].choices[j];
+         answerButton.setAttribute("data-user-choice", question[i].choice[j]);
+         answerButton.setAttribute("class", "answer-button");
+         answerChoicesDiv.appendChild(answerButton);
+      }
+
+      var answerButtonClass = document.getElementsByClassName("answer-button");
+      answerButtonClass.addEventListener("click", function () {
+
+         var userChoice = (document.querySelector(this).setAttribute("data-user-choice"));
+         if (userChoice === questions[i].answer) {
+            rightOrWrongDiv.innerHTML = "<hr><p>Correct</p>";
+         } else {
+            rightOrWrongDiv.innerHTML = "<hr><p>Wrong</p>";
+            totalSeconds = totalSeconds - 15;
+         }
+
+      }
+
+   }
+
+
+}
+
+// ************ EVENT LISTENERS ************
+
+
+// ***** EVENT LISTENER FOR START BUTTON *****
+startButton.addEventListener("click", startTimer);
+
+
+
+
+// ************ RUN FUNCTION *********
+
